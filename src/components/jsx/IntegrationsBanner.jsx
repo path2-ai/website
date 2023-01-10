eeb7891c4f75e05b732cf4a4e51abcf9f431f513
Johannes Hötter <johannes.hoetter@kern.ai>
2023-01-10 14:26:43 +0100

    mobile-friendly integrations banner

--- src/components/jsx/IntegrationsBanner.jsx
+++ src/components/jsx/IntegrationsBanner.jsx
@@ -6,27 +6,36 @@ const integrations = [
         name: "OpenAI",
         image: "openai",
         highlightE: true,
+        highlightEMobile: true,
+        highlightTMobile: true,
+        highlightLMobile: true,
     },
     {
         name: "GSheet",
         image: "gsheet",
         highlightE: true,
-
+        highlightEMobile: true,
+        highlightTMobile: true,
     },
     {
         name: "Webhook",
         image: "webhook",
         highlightE: true,
+        highlightEMobile: true,
+        highlightTMobile: true,
     },
     {
         name: "Slack",
         image: "slack",
         highlightT: true,
+        highlightEMobile: true,
+        highlightLMobile: true,
     },
     {
         name: "S3",
         image: "s3",
         highlightT: true,
+        highlightTMobile: true,
     },
     {
         name: "Twitter",
@@ -37,23 +46,31 @@ const integrations = [
         name: "Hubspot",
         image: "hubspot",
         highlightL: true,
+        highlightEMobile: true,
+        highlightLMobile: true,
     },
     {
         name: "Webflow",
         image: "webflow",
+        highlightEMobile: true,
+        highlightTMobile: true,
     },
     {
         name: "Zapier",
         image: "zapier",
+        highlightEMobile: true,
     },
     {
         name: "Discord",
         image: "discord",
         highlightE: true,
+        highlightEMobile: true,
+        highlightLMobile: true,
     },
     {
         name: "Gmail",
         image: "gmail",
+        highlightTMobile: true,
     },
     {
         name: "Airtable",
@@ -62,15 +79,22 @@ const integrations = [
     {
         name: "SurveyMonkey",
         image: "surveymonkey",
+        highlightEMobile: true,
+        highlightLMobile: true,
     },
     {
         name: "OpenAI",
         image: "openai",
         highlightT: true,
+        highlightEMobile: true,
+        highlightTMobile: true,
+        highlightLMobile: true,
     },
     {
         name: "Excel",
         image: "excel",
+        highlightEMobile: true,
+        highlightLMobile: true,
     },
     {
         name: "Intercom",
@@ -210,6 +234,8 @@ const integrations = [
     },
 ]
 
+const integrationsOnMobile = integrations.slice(0, 15)
+
 import { WORKFLOW_ASSETS_URL } from "./_settings"
 
 function classNames(...classes) {
@@ -237,46 +263,85 @@ export function IntegrationsBanner() {
 
 
     return (
-        <div>
-            <main className="">
+        <div className="mx-4">
+            <main >
                 <div className="relative px-6 max-w-5xl mx-auto lg:px-8">
                     <div className="opacity-0 opacity-20 opacity-40 opacity-60 opacity-80 opacity-100" /> {/* This is a hack to get the opacity classes to work */}
-                    <div className="grid grid-cols-9 gap-4 absolute mt-16 inset-0">
-                        {integrations.map((integration, index) => (
-                            <div key={index} className={classNames(
-                                "mx-auto w-fit h-fit p-6 rounded-2xl ring-1 ring-gray-500 shadow-[0_0px_20px_2px_rgba(255,255,255,0.1)]",
-                                (index % 9 == 4) ? (
-                                    'opacity-100'
-                                ) : (
-                                    index % 9 > 4 ? (
-                                        `opacity-${100 - Math.round((index % 9 / 5 + (1 / 5)) * 100) % 100}`
+                    <div className="hidden md:block">
+                        <div className="grid grid-cols-9 gap-4 absolute mt-16 inset-0">
+                            {integrations.map((integration, index) => (
+                                <div key={index} className={classNames(
+                                    "mx-auto w-fit h-fit p-6 rounded-2xl ring-1 ring-gray-500 shadow-[0_0px_20px_2px_rgba(255,255,255,0.1)]",
+                                    (index % 9 == 4) ? (
+                                        'opacity-100'
                                     ) : (
-                                        `opacity-${Math.round((index % 9 / 5 + (1 / 5)) * 100) % 100}`
-                                    )
-                                ),
-                                ((currentHighlighted == "E" && integration.highlightE) ? (
-                                    "bg-yellow-800 border border-yellow-500"
-                                ) : (currentHighlighted == "T" && integration.highlightT) ? (
-                                    "bg-blue-800 border border-blue-500"
-                                ) : (currentHighlighted == "L" && integration.highlightL) ? (
-                                    "bg-green-800 border border-green-500"
-                                ) : (
-                                    "bg-gradient-to-r from-gray-800 to-neutral-800"
-                                ))
-                            )}>
-                                <img
-                                    className="mx-auto animate-pulse"
-                                    src={`${WORKFLOW_ASSETS_URL}/icons/${integration.image}.png`}
-                                    alt=""
-                                    width="48"
-                                />
+                                        index % 9 > 4 ? (
+                                            `opacity-${100 - Math.round((index % 9 / 5 + (1 / 5)) * 100) % 100}`
+                                        ) : (
+                                            `opacity-${Math.round((index % 9 / 5 + (1 / 5)) * 100) % 100}`
+                                        )
+                                    ),
+                                    ((currentHighlighted == "E" && integration.highlightE) ? (
+                                        "bg-yellow-800 border border-yellow-500"
+                                    ) : (currentHighlighted == "T" && integration.highlightT) ? (
+                                        "bg-blue-800 border border-blue-500"
+                                    ) : (currentHighlighted == "L" && integration.highlightL) ? (
+                                        "bg-green-800 border border-green-500"
+                                    ) : (
+                                        "bg-gradient-to-r from-gray-800 to-neutral-800"
+                                    ))
+                                )}>
+                                    <img
+                                        className="mx-auto animate-pulse"
+                                        src={`${WORKFLOW_ASSETS_URL}/icons/${integration.image}.png`}
+                                        alt=""
+                                        width="48"
+                                    />
+                                </div>
+                            )
+                            )}
+                        </div>
+                        <div>
+                            <div className="absolute inset-0 bg-gradient-radial from-black via-neutral-900 to-transparent opacity-[.55]" />
+                            <div className="py-64 relative mx-auto rounded-2xl w-fit justify-center">
                             </div>
-                        )
-                        )}
+                        </div>
                     </div>
-                    <div>
-                        <div className="absolute inset-0 bg-gradient-radial from-black via-neutral-900 to-transparent opacity-[.55]" />
-                        <div className="py-64 relative mx-auto rounded-2xl w-fit justify-center">
+
+                    <div className="block md:hidden">
+                        <div className="grid grid-cols-3 gap-4 absolute mt-16 inset-0">
+                            {integrationsOnMobile.map((integration, index) => (
+                                <div key={index} className={classNames(
+                                    "mx-auto w-fit h-fit p-6 rounded-2xl ring-1 ring-gray-500 shadow-[0_0px_20px_2px_rgba(255,255,255,0.1)]",
+                                    (index % 3 == 1) ? (
+                                        'opacity-100'
+                                    ) : (
+                                        'opacity-75'
+                                    ),
+                                    ((currentHighlighted == "E" && integration.highlightEMobile) ? (
+                                        "bg-yellow-800 border border-yellow-500"
+                                    ) : (currentHighlighted == "T" && integration.highlightTMobile) ? (
+                                        "bg-blue-800 border border-blue-500"
+                                    ) : (currentHighlighted == "L" && integration.highlightLMobile) ? (
+                                        "bg-green-800 border border-green-500"
+                                    ) : (
+                                        "bg-gradient-to-r from-gray-800 to-neutral-800"
+                                    ))
+                                )}>
+                                    <img
+                                        className="mx-auto animate-pulse"
+                                        src={`${WORKFLOW_ASSETS_URL}/icons/${integration.image}.png`}
+                                        alt=""
+                                        width="48"
+                                    />
+                                </div>
+                            )
+                            )}
+                        </div>
+                        <div>
+                            <div className="absolute inset-0 bg-gradient-radial from-black via-neutral-900 to-transparent opacity-[.55]" />
+                            <div className="py-64 relative mx-auto rounded-2xl w-fit justify-center">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -287,8 +352,7 @@ export function IntegrationsBanner() {
                 </h1>
                 <div className="mt-10 flex flex-col text-gray-300">
                     <div className="text-lg flex flex-col mx-auto items-center">
-                        <div>Create pipelines for your natural language.</div>
-                        <div>Choose from the below extractors, transformers, and loaders.</div>
+                        <div>Create pipelines for your natural language. Choose from the below extractors, transformers, and loaders.</div>
                     </div>
                 </div>
             </div>
