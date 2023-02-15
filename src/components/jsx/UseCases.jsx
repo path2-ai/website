8a2df19ecc74bdd359bc0c66f419c500c69d907b
Johannes Hötter <johannes.hoetter@kern.ai>
2023-02-15 14:53:42 +0100

    adds max w

--- src/components/jsx/UseCases.jsx
+++ src/components/jsx/UseCases.jsx
@@ -142,9 +142,9 @@ export function UseCases() {
         </p>
       </div>
 
-      <div className="hidden lg:block overflow-hidden py-12 flex flex-col space-y-40 ">
+      <div className="max-w-5xl mx-auto hidden lg:block overflow-hidden py-12 flex flex-col space-y-40 ">
         {useCases.map((useCase, index) => (
-          <div id={useCase.slug} key={index} className="m-16 px-16 border border-gray-800 bg-neutral-900 rounded-2xl relative overflow-hidden">
+          <div id={useCase.slug} key={index} className="m-16 xl:my-16 xl:mx-0 px-16 border border-gray-800 bg-neutral-900 rounded-2xl relative overflow-hidden">
             {index % 2 === 0 ? (
               <div className="relative">
                 <svg
