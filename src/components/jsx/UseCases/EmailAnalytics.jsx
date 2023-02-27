91c566567c635f7efd1d54d09ce850d11e1ca729
Johannes Hötter <johannes.hoetter@kern.ai>
2023-02-27 23:49:00 +0100

    fix padding menu

--- src/components/jsx/UseCases/EmailAnalytics.jsx
+++ src/components/jsx/UseCases/EmailAnalytics.jsx
@@ -120,7 +120,7 @@ export function EmailAnalytics({ devIsSelected }) {
                                 </div>
                                 <Menu as="div" className="relative inline-block text-left">
                                     <div>
-                                        <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-900 px-4 py-2 text-sm font-medium text-gray-500 hover:text-gray-200 hover:bg-neutral-800 focus:outline-none">
+                                        <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-900 px-2 text-sm font-medium text-gray-500 hover:text-gray-200 hover:bg-neutral-800 focus:outline-none">
                                             Choose an example
                                             <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
                                         </Menu.Button>
