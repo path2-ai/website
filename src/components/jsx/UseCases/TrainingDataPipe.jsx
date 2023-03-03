2b9e7e5cf5c786878e88b6d88c69781b5e4100e6
Johannes Hötter <johannes.hoetter@kern.ai>
2023-03-03 01:19:21 +0100

    basic implementation training data pipeline

--- src/components/jsx/UseCases/TrainingDataPipe.jsx
+++ src/components/jsx/UseCases/TrainingDataPipe.jsx
@@ -1,37 +1,45 @@
 import { useEffect, useState } from 'react'
-import { IconArrowsShuffle } from '@tabler/icons'
+import { IconArrowsShuffle, IconPlayerPause, IconPlayerPlay } from '@tabler/icons'
 import { Circular } from '@/util/Circular'
+import { Tooltip } from "@nextui-org/react";
+
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-const slider = new Circular([
+const slideContent = [
     {
         name: 'workflows',
+        title: 'Design workflows',
         video: "/video/quickstart.mp4",
     },
     {
         name: 'annotate',
+        title: 'Annotate data',
         video: "/video/quickstart.mp4",
     },
     {
         name: 'manage',
+        title: 'Manage and orchestrate',
         video: "/video/quickstart.mp4",
     },
     {
         name: 'automate',
+        title: 'Build automations',
         video: "/video/quickstart.mp4",
     },
     {
         name: 'deploy',
+        title: 'Deploy as an API',
         video: "/video/quickstart.mp4",
     }
-])
+]
+const slideOptions = Object.entries(slideContent).map(([key, value]) => value.name)
+const slider = new Circular(slideOptions)
 
 export function TrainingDataPipe() {
 
-    const [showFeature, setShowFeature] = useState("workflows")
 
 
     const [current, setCurrent] = useState(slider.current())
@@ -49,9 +57,6 @@ export function TrainingDataPipe() {
         return () => clearInterval(interval)
     }, [timer])
 
-    useEffect(() => {
-        setShowFeature(current.name)
-    }, [current])
 
     return (
         <div className='my-20'>
@@ -71,125 +76,107 @@ export function TrainingDataPipe() {
                         </p>
                     </div>
                 </div>
-                <div className='mx-auto w-fit p-[1px] rounded-2xl bg-gradient-to-r from-gray-900 via-gray-600 to-black'>
-                    <div className='flex flex-row space-x-2 items-center justify-between p-2 text-gray-500 text-sm rounded-2xl bg-neutral-900'>
-
-                        <div className='w-fit p-[1px] rounded-xl bg-gradient-to-r from-gray-800 to-gray-700'>
-                            <div
-                                className=' p-2 bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900 rounded-xl hover:from-neutral-800 hover:via-neutral-700 hover:to-neutral-800 hover:text-white'
-                            >
-
-                                <button
-                                    className={classNames(
-                                        showFeature == 'workflows' && 'text-white',
-                                        'flex flex-row items-center'
-                                    )}
-                                    onClick={() => setShowFeature("workflows")}
-                                >
-                                    <span
+                <div className='flex flex-row items-center space-x-2 mx-auto '>
+                    <div className='w-fit p-[1px] rounded-2xl bg-gradient-to-r from-gray-900 via-gray-600 to-black'>
+                        <div className='flex flex-row space-x-2 items-center justify-between p-2 text-gray-500 text-sm rounded-2xl bg-neutral-900'>
+                            {slideContent.map((item) => (
+                                <div className='w-fit p-[1px] rounded-xl bg-gradient-to-r from-gray-800 to-gray-700'>
+                                    <div
                                         className={classNames(
-                                            'mt-0.5 rounded-full w-2 h-2 inline-block mx-1.5',
-                                            showFeature == 'workflows' ? 'bg-green-500' : 'bg-neutral-500'
+                                            clickedOption === item.name ? 'border border-green-500' : 'border border-transparent',
+                                            'p-2 bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900 rounded-xl hover:from-neutral-800 hover:via-neutral-700 hover:to-neutral-800 hover:text-white cursor-pointer'
                                         )}
-                                    />
-                                    <span>
-                                        Design workflows
-                                    </span>
-                                </button>
-                                <div
-                                    className='mt-1 w-full bg-gray-200 h-1.5 rounded-full'
-                                >
-                                    {showFeature == 'workflows' && (
+                                        onClick={() => {
+                                            if (item.name === clickedOption) {
+                                                setClickedOption(null)
+                                                setTimer(0)
+                                                return
+                                            }
+                                            while (slider.current() !== item.name) {
+                                                slider.next()
+                                            }
+                                            setCurrent(item.name)
+                                            setClickedOption(item.name)
+                                            setTimer(0)
+                                        }}
+                                    >
+
+                                        <button
+                                            className={classNames(
+                                                current == item.name && 'text-white',
+                                                'flex flex-row items-center'
+                                            )}
+
+                                        >
+                                            <span
+                                                className={classNames(
+                                                    'mt-0.5 rounded-full w-2 h-2 inline-block mx-1.5',
+                                                    current == item.name ? 'bg-green-500' : 'bg-neutral-500'
+                                                )}
+                                            />
+                                            <span>
+                                                {item.title}
+                                            </span>
+                                        </button>
                                         <div
-                                            className='bg-green-500 h-1.5 rounded-full' style={{ width: `${timer / 5}%` }}
-                                        />
-                                    )}
+                                            className={classNames(
+                                                current == item.name ? 'bg-gray-200' : 'bg-gradient-to-r from-neutral-800 via-neutral-700 to-neutral-800',
+                                                'mt-1 w-full h-1.5 rounded-full'
+                                            )}
+                                        >
+                                            {current == item.name && !clickedOption && (
+                                                <div
+                                                    className='bg-green-500 h-1.5 rounded-full' style={{ width: `${timer / 5}%` }}
+                                                />
+                                            )}
 
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            ))}
 
-                        </div>
 
-                        <div className='w-fit p-[1px] rounded-xl bg-gradient-to-r from-gray-700 via-gray-500 to-gray-600'>
-                            <button
-                                className={classNames(
-                                    showFeature == 'annotate' && 'text-white',
-                                    'flex flex-row items-center p-2 bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900 rounded-xl hover:from-neutral-800 hover:via-neutral-700 hover:to-neutral-800 hover:text-white'
-                                )}
-                                onClick={() => setShowFeature('annotate')}
-                            >
-                                <span
-                                    className={classNames(
-                                        'mt-0.5 rounded-full w-2 h-2 inline-block mx-1.5',
-                                        showFeature == 'annotate' ? 'bg-green-500' : 'bg-neutral-500'
-                                    )}
-                                />
-                                <span>
-                                    Annotate data
-                                </span>
-                            </button>
-                        </div>
-
-                        <div className='w-fit p-[1px] rounded-xl bg-gradient-to-r from-gray-600 via gray-500 to-gray-600'>
-                            <button
-                                className={classNames(
-                                    showFeature == 'manage' && 'text-white',
-                                    'flex flex-row items-center p-2 bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900 rounded-xl hover:from-neutral-800 hover:via-neutral-700 hover:to-neutral-800 hover:text-white'
-                                )}
-                                onClick={() => setShowFeature('manage')}
-                            >
-                                <span
-                                    className={classNames(
-                                        'mt-0.5 rounded-full w-2 h-2 inline-block mx-1.5',
-                                        showFeature == 'manage' ? 'bg-green-500' : 'bg-neutral-500'
-                                    )}
-                                />
-                                <span>
-                                    Manage and orchestrate
-                                </span>
-                            </button>
-                        </div>
-
-                        <div className='w-fit p-[1px] rounded-xl bg-gradient-to-r from-gray-600 to-neutral-700'>
-                            <button
-                                className={classNames(
-                                    showFeature == 'automate' && 'text-white',
-                                    'flex flex-row items-center p-2 bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900 rounded-xl hover:from-neutral-800 hover:via-neutral-700 hover:to-neutral-800 hover:text-white'
-                                )}
-                                onClick={() => setShowFeature('automate')}
-                            >
-                                <span
-                                    className={classNames(
-                                        'mt-0.5 rounded-full w-2 h-2 inline-block mx-1.5',
-                                        showFeature == 'automate' ? 'bg-green-500' : 'bg-neutral-500'
-                                    )}
-                                />
-                                <span>
-                                    Build automations
-                                </span>
-                            </button>
-                        </div>
-
-                        <div className='w-fit p-[1px] rounded-xl bg-gradient-to-r from-neutral-700 to-neutral-800'>
-                            <button
-                                className={classNames(
-                                    showFeature == 'deploy' && 'text-white',
-                                    'flex flex-row items-center p-2 bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900 rounded-xl hover:from-neutral-800 hover:via-neutral-700 hover:to-neutral-800 hover:text-white'
-                                )}
-                                onClick={() => setShowFeature('deploy')}
-                            >
-                                <span
-                                    className={classNames(
-                                        'mt-0.5 rounded-full w-2 h-2 inline-block mx-1.5',
-                                        showFeature == 'deploy' ? 'bg-green-500' : 'bg-neutral-500'
-                                    )}
-                                />
-                                <span>
-                                    Deploy as API
-                                </span>
-                            </button>
                         </div>
                     </div>
+                    {clickedOption && (
+                        <div className="-mt-2">
+                            <Tooltip className='ml-1' color="invert" content={"Autoplay"} placement="right"
+                            >
+                                <button
+                                    type='button'
+                                    className='mt-2 h-fit hover:text-green-500 hover:bg-neutral-800 bg-neutral-900 flex rounded-lg p-2 text-xs font-semibold leading-7 text-gray-100 ring-1 ring-inset ring-neutral-700 group-hover:ring-white/20'
+                                    onClick={() => {
+                                        setClickedOption(null)
+                                        setTimer(0)
+                                    }}
+                                >
+                                    <IconPlayerPlay className='my-auto h-5 w-5 inline-block' />
+                                </button>
+                            </Tooltip>
+                        </div>
+                    )}
+                    {clickedOption == null && (
+                        <div className='-mt-2'>
+                            <Tooltip className='ml-1' color="invert" content={"Stop autoplay"} placement="right"
+                            >
+                                <button
+                                    type='button'
+                                    className='mt-2 h-fit hover:text-red-400 hover:bg-neutral-800 bg-neutral-900 flex rounded-lg p-2 text-xs font-semibold leading-7 text-gray-100 ring-1 ring-inset ring-neutral-700 group-hover:ring-white/20'
+                                    onClick={() => {
+                                        while (slider.current() !== current) {
+                                            slider.next()
+                                        }
+                                        setCurrent(current)
+                                        setClickedOption(current)
+                                        setTimer(0)
+                                    }}
+                                >
+
+                                    <IconPlayerPause className='my-auto h-5 w-5 inline-block' />
+                                </button>
+                            </Tooltip>
+                        </div>
+                    )}
                 </div>
             </div>
 
@@ -206,7 +193,9 @@ export function TrainingDataPipe() {
                         loop
                         muted
                     >
-                        <source src={current.video} type="video/mp4"
+                        <source src={
+                            slideContent.find((item) => item.name === current).video
+                        } type="video/mp4"
                         />
                         Sorry, your browser doesn't support videos.
                     </video>
